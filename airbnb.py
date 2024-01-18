3168270f7c4d2702f4077cd126c9dcbbc93855ae
neal lockhart <neal301@gmail.com>
2024-01-17 20:17:58 -0500

    fixed numeric and categorical columns

--- airbnb.py
+++ airbnb.py
@@ -19,8 +19,12 @@ from sklearn.model_selection import train_test_split
 train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
 listings = train_set[['price', 'host_is_superhost', 'host_listings_count', 'host_total_listings_count', 'latitude', 'longitude', 'property_type', 
-          'accommodates', 'beds', 'minimum_nights', 'maximum_nights', 'number_of_reviews', 'review_scores_rating', 'review_scores_cleanliness', 
+          'accommodates', 'bathrooms_text', 'beds', 'minimum_nights', 'maximum_nights', 'number_of_reviews', 'review_scores_rating', 'review_scores_cleanliness', 
           'review_scores_communication', 'review_scores_value']]
+
+#%%
+
+
 
 #%%
 
@@ -28,18 +32,32 @@ def clean_price(price_column):
     #cleans and converts price to strings  
     price_column = price_column.str.strip('$').str.replace(',', '').reset_index()['price']
     price_column = price_column.str[:-3]
-    price_column = price_column.astype(int)
+    price_column = pd.to_numeric(price_column, errors='raise')
     return price_column
 
 listings['price'] = clean_price(listings['price'])
+listings = listings[(listings['price'] < listings['price'].quantile(.99)) & (listings['price'] > listings['price'].quantile(.01))]
 
-listings = listings[listings['price'] < listings['price'].quantile(.95)]
 
 def superhost_numeric(host_is_superhost):
+    #converts this column to numeric
     host_is_superhost = host_is_superhost.map({'f':0, 't':1})
     return host_is_superhost
 
 listings['host_is_superhost'] = superhost_numeric(listings['host_is_superhost'])
+
+
+def clean_bathrooms(bathrooms_text):
+    #cleans the bathrooms... har har har... no actually it cleans the bathrooms_text column
+    pattern = r'(\d.?\d?)\s'
+    pattern2 = r'(Half)'
+    bathrooms_text[bathrooms_text.str.contains(pattern2, na=False)] = .5
+    bathrooms_text = bathrooms_text.str.extract(pattern)
+    return bathrooms_text
+
+listings['bathrooms'] = clean_bathrooms(listings['bathrooms_text'])
+listings.drop(columns='bathrooms_text', inplace=True)
+
 
 def clean_property_type(property_type_col):
     #consolidate the property_type var to a few common categories   
@@ -54,22 +72,24 @@ def clean_property_type(property_type_col):
 
 listings['property_type'] = clean_property_type(listings['property_type'])
 
+#%%
+
+listings.plot.scatter(x='longitude', y='latitude', grid=True,
+                      c='price', cmap='jet', colorbar=True)
 
 #%%
 
-test.hist(bins=50)
+from sklearn.impute import SimpleImputer
 
+imputer = SimpleImputer(strategy='mean')
 
+listings_num = listings.select_dtypes(include='number')
+imputer.fit(listings_num)
+listings_num = imputer.transform(listings_num)
 
 #%%
 
-listings[listings['review_scores_value'] == 1]
-
-#%%
+listings.columns
 
 
-listings.plot.scatter(x = 'longitude', y = 'latitude', grid=True, 
-                       c='price', cmap='jet', colorbar=True,
-                      legend=True, figsize=(10,7))
-
-
+# %%
